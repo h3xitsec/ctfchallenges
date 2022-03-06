ee51231ff8049bcb0cd0177770a325327dc626c7
h3xit <h3xit@protonmail.com>
2022-03-06 14:04:14 -0500

    blue solve script

--- umdctf/forensics/blue/solve.py
+++ umdctf/forensics/blue/solve.py
@@ -9,6 +9,8 @@ def get_line(img,line):
         pixels.append(img.getpixel((x,line)))
     return pixels
 
+# get the number of char in the flag
+# since the stegano script modify the first 'charcount' lines of the image, the last one is untouched
 def get_charcount(img):
     width, height = img.size
     count = 0
@@ -23,7 +25,12 @@ def get_charcount(img):
 # open image
 img = Image.open('bluer.png')
 width, height = img.size
-# get total for lastline as a reference
+
+# the script modifies a numbers of pixel equals to the charcode (a = 97 = 97 pixels modified)
+# the modification is always the same : +1 on one of the 3 colors
+# we have to count the number of increment that happenened to get the charcode
+
+# count the total for the last line and use it as a reference
 lastline = get_line(img,height - 1)
 lastlinetotal = 0
 for i in range(len(lastline)):
@@ -31,12 +38,12 @@ for i in range(len(lastline)):
     lastlinetotal = lastlinetotal + (pixel[0] + pixel[1] + pixel[2])
 ref = lastlinetotal
 
-# count number or char in flag
+# count number of char in the flag
 charcount = get_charcount(img)
 print('Flag has ' + str(charcount) + ' chars')
 
 # get total for each line from start to 'charcount' substract reference
-# this number represent the number of increment that happened on the line AND the charcode
+# this number represent the number of increment that happened on the line AND the charcode for the flag
 flag = ''
 for y in range(charcount):
     line = get_line(img,y)
